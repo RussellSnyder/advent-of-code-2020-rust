2a35813b25ef3a12300a5c55ed34ad6f20d1131e
Frank Blendinger <frank.blendinger@methodpark.de>
2020-12-02 16:07:19 +0100

    Implement day 2 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -6,7 +6,7 @@ pub fn part1(inp: String) {
 
     let valid_passwords = lines.iter()
         .filter(|line| line.len() > 0)
-        .map(|x| is_valid(x, &regex))
+        .map(|x| is_valid_1(x, &regex))
         .fold(0, |acc, cur| acc + match cur {
             true  => 1,
             false => 0,
@@ -16,12 +16,22 @@ pub fn part1(inp: String) {
 }
 
 pub fn part2(inp: String) {
-    // TODO
+    let lines = inp.split("\n").collect::<Vec<&str>>();
+    let regex = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
+
+    let valid_passwords = lines.iter()
+        .filter(|line| line.len() > 0)
+        .map(|x| is_valid_2(x, &regex))
+        .fold(0, |acc, cur| acc + match cur {
+            true  => 1,
+            false => 0,
+        });
+
+    println!("{}", valid_passwords);
 }
 
-
 // sample: 1-3 a: abaaabab
-fn is_valid(line: &str, regex: &Regex) -> bool {
+fn is_valid_1(line: &str, regex: &Regex) -> bool {
     for cap in regex.captures_iter(line) {
         let min    = cap[1].parse::<usize>().unwrap();
         let max    = cap[2].parse::<usize>().unwrap();
@@ -31,6 +41,28 @@ fn is_valid(line: &str, regex: &Regex) -> bool {
         let letters_in_passwd = passwd.matches(letter).count();
 
         return min <= letters_in_passwd && letters_in_passwd <= max;
+    }
+
+    false
+}
+
+fn is_valid_2(line: &str, regex: &Regex) -> bool {
+    for cap in regex.captures_iter(line) {
+        let pos1   = cap[1].parse::<usize>().unwrap() - 1;
+        let pos2   = cap[2].parse::<usize>().unwrap() - 1;
+        let letter = cap[3].as_bytes()[0];
+        let passwd = cap[4].as_bytes();
+
+        let mut matches = 0;
+
+        if pos1 < passwd.len() && passwd[pos1] == letter {
+            matches += 1;
+        }
+        if pos2 < passwd.len() && passwd[pos2] == letter {
+            matches += 1;
+        }
+
+        return matches == 1;
     }
 
     false

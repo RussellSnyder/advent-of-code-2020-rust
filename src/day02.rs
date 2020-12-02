920d52013f1baf9abd328bd261ab96ee8ca6a7c3
Frank Blendinger <frank.blendinger@methodpark.de>
2020-12-02 15:52:49 +0100

    Extract regex to drastically improve performance

--- src/day02.rs
+++ src/day02.rs
@@ -2,10 +2,11 @@ use regex::Regex;
 
 pub fn part1(inp: String) {
     let lines = inp.split("\n").collect::<Vec<&str>>();
+    let regex = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
 
     let valid_passwords = lines.iter()
         .filter(|line| line.len() > 0)
-        .map(|x| is_valid(x))
+        .map(|x| is_valid(x, &regex))
         .fold(0, |acc, cur| acc + match cur {
             true  => 1,
             false => 0,
@@ -18,10 +19,9 @@ pub fn part2(inp: String) {
     // TODO
 }
 
-// sample: 1-3 a: abaaabab
-fn is_valid(line: &str) -> bool {
-    let regex = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
 
+// sample: 1-3 a: abaaabab
+fn is_valid(line: &str, regex: &Regex) -> bool {
     for cap in regex.captures_iter(line) {
         let min    = cap[1].parse::<usize>().unwrap();
         let max    = cap[2].parse::<usize>().unwrap();
